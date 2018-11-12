20cad12be950fae3893dc92d6fe2d2e88087a706
Ryan Wilson <rwilson@ucvts.org>
2018-11-12 08:57:50 -0500

    initial commit

---
+++ js/problem-set-4.js
@@ -0,0 +1,213 @@
+/*
+ * Hello. 2 points.
+ */
+
+function hello() {
+
+  // WRITE YOUR EXERCISE 1 CODE HERE
+
+  ////////////////// DO NOT MODIFY
+  check("hello"); // DO NOT MODIFY
+  ////////////////// DO NOT MODIFY
+}
+
+/*
+ * Hello, Again. 3 points.
+ */
+
+function helloAgain() {
+
+  //////////// DO NOT MODIFY      Use the @name variable to store
+  let name; // DO NOT MODIFY      the value that the user enters
+  //////////// DO NOT MODIFY      in response to your prompt.
+
+  // WRITE YOUR EXERCISE 2 CODE HERE
+
+  ///////////////////////////// DO NOT MODIFY
+  check("helloAgain", name); // DO NOT MODIFY
+  ///////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Celsius. 5 points.
+ */
+
+function celsius() {
+
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+  let cels = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // The above code generates a random number between -100 and 1000
+  // (inclusive), and rounds this value to 2 decimal places.
+
+  // WRITE YOUR EXERCISE 3 CODE HERE
+
+  ////////////////////////// DO NOT MODIFY
+  check("celsius", cels); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Fahrenheit. 5 points.
+ */
+
+function fahrenheit() {
+
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+  let fahr = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
+  //////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // The above code generates a random number between -100 and 1000
+  // (inclusive), and rounds this value to 2 decimal places.
+
+  // WRITE YOUR EXERCISE 4 CODE HERE
+
+  ///////////////////////////// DO NOT MODIFY
+  check("fahrenheit", fahr); // DO NOT MODIFY
+  ///////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Inches. 5 points.
+ */
+
+function inches() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of @inches, which you'll likely
+  // need to do. Please do not modify the value of @input.
+
+  ////////////////////// DO NOT MODIFY
+  let inches = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 5 CODE HERE
+
+  ////////////////////////// DO NOT MODIFY
+  check("inches", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Centimeters. 5 points.
+ */
+
+function centimeters() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of @centimeters, which you'll
+  // likely need to do. Please do not modify the value of @input.
+
+  /////////////////////////// DO NOT MODIFY
+  let centimeters = input; // DO NOT MODIFY
+  /////////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 6 CODE HERE
+
+  /////////////////////////////// DO NOT MODIFY
+  check("centimeters", input); // DO NOT MODIFY
+  /////////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Fluid Ounces. 5 points.
+ */
+
+function fluidOunces() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of @fluidOunces, which you'll
+  // likely need to do. Please do not modify the value of @input.
+
+  /////////////////////////// DO NOT MODIFY
+  let fluidOunces = input; // DO NOT MODIFY
+  /////////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 7 CODE HERE
+
+  /////////////////////////////// DO NOT MODIFY
+  check("fluidOunces", input); // DO NOT MODIFY
+  /////////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Ounces. 5 points.
+ */
+
+function ounces() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of @ounces, which you'll likely
+  // need to do. Please do not modify the value of @input.
+
+  ////////////////////// DO NOT MODIFY
+  let ounces = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 8 CODE HERE
+
+  ////////////////////////// DO NOT MODIFY
+  check("ounces", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Money. 7 points.
+ */
+
+function money() {
+
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  /////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of @pennies, which you'll likely
+  // need to do. Please do not modify the value of @input.
+
+  /////////////////////// DO NOT MODIFY
+  let pennies = input; // DO NOT MODIFY
+  /////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 9 CODE HERE
+
+  ///////////////////////// DO NOT MODIFY
+  check("money", input); // DO NOT MODIFY
+  ///////////////////////// DO NOT MODIFY
+}
+
+/*
+ * Change. 8 points.
+ */
+
+function change() {
+
+  ///////////////////////////////////////////////////////////////////// DO NOT MODIFY
+  let input = prompt("Enter a non-negative number less than 1.00."); // DO NOT MODIFY
+  ///////////////////////////////////////////////////////////////////// DO NOT MODIFY
+
+  // You are free to modify the value of @amount, which you'll likely
+  // need to do. Please do not modify the value of @input.
+
+  ////////////////////// DO NOT MODIFY
+  let amount = input; // DO NOT MODIFY
+  ////////////////////// DO NOT MODIFY
+
+  // WRITE YOUR EXERCISE 10 CODE HERE
+
+  ////////////////////////// DO NOT MODIFY
+  check("change", input); // DO NOT MODIFY
+  ////////////////////////// DO NOT MODIFY
+}
